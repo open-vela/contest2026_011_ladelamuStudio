0e4c557b4c80bfa2feac42e7e04ae5ee248e2094
lladlam <lladlam@outlook.com>
2026-07-13 23:04:21 +0800

    feat: D13x Hengshan Pi BSP 初版
    - 芯片层: artinchip_start.c, _irq.c, _lowputc.c, _uart.c, _allocateheap.c
    - 板级层: defconfig, ld.script, board.h, _boot.c, _appinit.c
    - 编译通过: nuttx.bin 209KB
    - 工具链: xPack RISC-V GCC 14.2.0
    
    Signed-off-by: lladlam <lladlam@users.noreply.github.com>

---
+++ board/d13x-hengshan-pi/include/board.h
@@ -0,0 +1,27 @@
+/****************************************************************************
+ * vendor/artinchip/boards/d13x-hengshan-pi/include/board.h
+ *
+ * D13x Hengshan Pi 板级宏定义
+ *
+ ****************************************************************************/
+
+#ifndef __VENDOR_ARTINCHIP_BOARDS_D13X_HENGSHAN_PI_INCLUDE_BOARD_H
+#define __VENDOR_ARTINCHIP_BOARDS_D13X_HENGSHAN_PI_INCLUDE_BOARD_H
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* 系统时钟频率 */
+
+#define BOARD_SYSCLK              480000000
+#define BOARD_AHBCLK              200000000
+#define BOARD_APB0CLK             100000000
+#define BOARD_UARTCLK             60000000
+
+/* 板级标识 */
+
+#define BOARD_NAME                "D13x Hengshan Pi"
+#define BOARD_VERSION             "1.0"
+
+#endif /* __VENDOR_ARTINCHIP_BOARDS_D13X_HENGSHAN_PI_INCLUDE_BOARD_H */
